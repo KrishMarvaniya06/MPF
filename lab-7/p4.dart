77d3db91341849b35945670bb95381a9a5705543
KrishMarvaniya06 <23020201414@darshan.ac.in>
2025-07-02 11:11:17 +0530

    Update p4.dart

--- lab-7/p4.dart
+++ lab-7/p4.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String imageUrl =
-      'https://images.unsplash.com/photo-1547721064-da6cfb341d50'; // Replace with your own URL
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cached Image Example',
+      title: 'Stack Widget Example',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cached Network Image'),
-          backgroundColor: Colors.blueAccent,
-        ),
+        appBar: AppBar(title: Text('Text on Image using Stack')),
         body: Center(
-          child: CachedNetworkImage(
-            imageUrl: imageUrl,
-            placeholder: (context, url) => CircularProgressIndicator(),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-            height: 300,
-            width: 300,
-            fit: BoxFit.cover,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.network(
+                'https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2@1.5x.png', // Replace with your own image URL
+                width: 300,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
