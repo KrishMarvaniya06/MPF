4c39c4249b9e4d7f01f66640ea5aa9985869597e
KrishMarvaniya06 <23020201414@darshan.ac.in>
2025-07-23 11:04:47 +0530

    lab-9

---
+++ lab-9/9.2.dart
@@ -0,0 +1,48 @@
+// 9.2 -  W.A.P. to Build a quote Generator App using Column, Text, ElevatedButton, and SnackBar (B)
+
+import 'package:flutter/material.dart';
+class NineTwo extends StatefulWidget {
+  const NineTwo({super.key});
+
+  @override
+  State<NineTwo> createState() => _NineTwoState();
+}
+
+class _NineTwoState extends State<NineTwo> {
+
+  Widget quoteCard({required String qoute,
+    String? author}){
+    return Card(
+      child: ListTile(
+        title: Text(qoute,style: TextStyle(
+          fontWeight: FontWeight.bold,),),
+        subtitle: Text(
+            "Author :: ${author ?? "Unknown"} "),
+        trailing: IconButton(onPressed: (){
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("copy to clipboard")));
+        }, icon: Icon(Icons.copy)),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              quoteCard(qoute: "Hello Darshan University"),
+              quoteCard(qoute: "Hello Darshan University"),
+              quoteCard(qoute: "Hello Darshan University"),
+              quoteCard(qoute: "Hello Darshan University"),
+              quoteCard(qoute: "Hello Darshan University"),
+              quoteCard(qoute: "Hello Darshan University"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
