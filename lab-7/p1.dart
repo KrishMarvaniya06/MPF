5714cd6519184fb0557803718f8b6425a7aaf8c2
KrishMarvaniya06 <23020201414@darshan.ac.in>
2025-08-03 17:15:54 +0530

    Update p1.dart

--- lab-7/p1.dart
+++ lab-7/p1.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,3 +15,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
